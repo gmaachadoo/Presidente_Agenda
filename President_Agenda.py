ed7d1903b07fcf3e75c7062947f5e13683f5b085
Gustavo Machado <guguisnm2004@gmail.com>
2024-08-26 16:09:54 -0300

    adicionar parametros no código de data_start e end_date

--- President_Agenda.py
+++ President_Agenda.py
@@ -2,44 +2,57 @@ import argparse
 import requests
 from bs4 import BeautifulSoup
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
-parser = argparse.ArgumentParser(description='Extrair compromissos para uma data específica.')
-parser.add_argument('--data', type=str, required=True, help='Data no formato DD/MM/AAAA')
+
+def convert_date(date_str):
+    try:
+        return datetime.strptime(date_str, '%d/%m/%Y')
+    except ValueError:
+        print("Formato de data inválido. Use o formato DD/MM/AAAA.")
+        exit(1)
+
+
+parser = argparse.ArgumentParser(description="Extrair compromissos das datas específicas.")
+parser.add_argument("--start_date", type=str, required=True, help='Data no formato DD/MM/AAAA')
+parser.add_argument("--end_date", type=str, required=True, help='Data no formato DD/MM/AAAA')
 
 args = parser.parse_args()
-data_str = args.data
+start_date = convert_date(args.start_date)
+end_date = convert_date(args.end_date)
 
-try:
-    data = datetime.strptime(data_str, '%d/%m/%Y').strftime('%Y-%m-%d')
-except ValueError:
-    print("Formato de data inválido. Use o formato DD/MM/AAAA.")
+if start_date > end_date:
+    print("A data de início não pode ser posterior à data de fim.")
     exit(1)
 
-
-url = f'https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/agenda-do-presidente-da-republica-lula/agenda-do-presidente-da-republica/{data}'
-
-
-response = requests.get(url)
-content = response.content
-
-site = BeautifulSoup(content, 'html.parser')
-
-commitments = site.findAll('li', class_='item-compromisso-wrapper')
 commitments_list = []
 
-for commitment in commitments:
-    title = commitment.find('h2', class_='compromisso-titulo').text
-    time = commitment.find('time', class_='compromisso-inicio').text
-    location = commitment.find('div', class_='compromisso-local').text
+current_date = start_date
+
+while current_date <= end_date:
+    data = current_date.strftime('%Y-%m-%d')
     
+    url = f'https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/agenda-do-presidente-da-republica-lula/agenda-do-presidente-da-republica/{data}'
+    response = requests.get(url)
+    content = response.content
+
+    site = BeautifulSoup(content, 'html.parser')
+    commitments = site.findAll('li', class_='item-compromisso-wrapper')
+
+    for commitment in commitments: # vou adicionar esse for dentro de um outro for que vai alterando até a data desejada
+        title = commitment.find('h2', class_='compromisso-titulo').text
+        time = commitment.find('time', class_='compromisso-inicio').text
+        location = commitment.find('div', class_='compromisso-local').text
+        
+        
+        commitments_list.append({
+            'titulo': title,
+            'horario': time,
+            'local': location
+        })
     
-    commitments_list.append({
-        'titulo': title,
-        'horario': time,
-        'local': location
-    })
-    
+    current_date += timedelta(days=1)
+
 
 with open('meu_arquivo_v1.json', 'w', encoding='utf-8') as arquivo_json:
     json.dump(commitments_list, arquivo_json, ensure_ascii=False, indent=4)
